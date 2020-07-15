781935f50c64cfc855bf4359b60656f87cb832f6
Rayan Hatout <rayan.hatout@gmail.com>
2020-07-15 22:59:11 +0100

    Add a basic button

--- public/client.js
+++ public/client.js
@@ -1,9 +1,12 @@
-TrelloPowerUp.initialize({
-  'board-buttons': function(t, options){
-    return [{
-      text: 'Inspiring Boards',
-      url: 'https://trello.com/inspiration',
-      target: 'Inspiring Boards'
-    }];
-  }
+const draftEmail = (t, opts) => {
+    console.log('Is this working?????');
+};
+
+window.TrelloPowerUp.initialize({
+    'card-buttons': function (t, options) {
+        return [{
+            text: 'Draft Email',
+            callback: draftEmail,
+        }];
+    }
 });
