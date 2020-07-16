3b3d09b65c5ebc5072e58643f50070467f7a63eb
Rayan Hatout <rayan.hatout@gmail.com>
2020-07-16 11:18:10 +0100

    Replace the callback by a dead-simple one

--- public/client.js
+++ public/client.js
@@ -1,19 +1,23 @@
-const draftEmail = function (t) {
-    console.log("Test")
-    fetch("https://docsoc-ex-machina.herokuapp.com/trello", {
-        method: "POST",
-        headers: {
-            "contentType": "application/json"
-        },
-        body: JSON.stringify(t)
-    })
+// const draftEmail = function (t) {
+//     console.log("Test")
+//     fetch("https://docsoc-ex-machina.herokuapp.com/trello", {
+//         method: "POST",
+//         headers: {
+//             "contentType": "application/json"
+//         },
+//         body: JSON.stringify(t)
+//     })
+// };
+
+var onBtnClick = function (t, opts) {
+    console.log('Someone clicked the button');
 };
 
 TrelloPowerUp.initialize({
     'card-buttons': function (t, options) {
         return [{
             text: 'Draft Email',
-            callback: draftEmail,
+            callback: onBtnClick
         }];
     }
 });
