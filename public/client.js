71ff8d1c9d4db06778a6e34a0c24dd92869ae83a
Rayan Hatout <rayan.hatout@gmail.com>
2020-07-16 13:06:11 +0100

    Update the callback

--- public/client.js
+++ public/client.js
@@ -1,16 +1,17 @@
-// const draftEmail = function (t) {
-//     console.log("Test")
-//     fetch("https://docsoc-ex-machina.herokuapp.com/trello", {
-//         method: "POST",
-//         headers: {
-//             "contentType": "application/json"
-//         },
-//         body: JSON.stringify(t)
-//     })
-// };
+var draftEmail = function (t, opts) {
+    console.log("Test")
+    fetch("https://docsoc-ex-machina.herokuapp.com/trello", {
+        method: "POST",
+        headers: {
+            "contentType": "application/json"
+        },
+        body: JSON.stringify(t)
+    })
+};
 
 var onBtnClick = function (t, opts) {
     console.log('Someone clicked the button');
+    draftEmail(t, opts);
 };
 
 TrelloPowerUp.initialize({
